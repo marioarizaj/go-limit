11eb317cb6aca471c119df4f96eaa009d49ff57e
mario <marizaj@trailblazer.ai>
2018-12-17 19:20:42 +0100

    --added comments

--- limiter.go
+++ limiter.go
@@ -8,17 +8,21 @@ import (
 var req chan int
 
 func main() {
+	//limit is a channel that will hold the processed requests
 	limit := make(chan int, 2)
+	//ticker is a channel that will receive a value every 10 seconds
 	ticker := time.NewTicker(10 * time.Second)
 	req = make(chan int)
 	var willBeEmpty = time.Now().Add(10 * time.Second)
 	go func() {
 		for {
 			select {
+			// When the time that we want to limit the resources has passed me empty the limit channel
 			case <-ticker.C:
 				willBeEmpty = time.Now().Add(10 * time.Second)
 				limit = make(chan int, 2)
 			case <-req:
+				//Here we check if the number of requests we processed is the same as the limit in the specified time range
 				if len(limit) != cap(limit) {
 					fmt.Println("Limit not reached")
 					limit <- 1
@@ -28,6 +32,7 @@ func main() {
 			}
 		}
 	}()
+	// This is just to test if the limiter works
 	i := 0
 	for {
 		req <- i
